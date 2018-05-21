93497ad741a86be0b16cd9b152dfad2f3fe838fb
hayatata <hayatata@live.jp>
2018-05-21 22:53:51 +0900

    お金のDollarクラスの掛け算のテストを実装。 Dollarクラスが存在しないため、コンパイルすらできない。

---
+++ TestDrivenDevelopmentUnitTest/MoneyTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestDrivenDevelopmentUnitTest
+{
+    [TestClass]
+    public class MoneyTest
+    {
+        [TestMethod]
+        public void TestMultiplication()
+        {
+            Dollar five = new Dollar(5);
+            five.times(2);
+            Assert.AreEqual(10, five.amount);
+        
+        }
+    }
+}
