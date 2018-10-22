415a855a2ddd65069f184f1986d0fb10761f6e58
hayatata <hayatata@live.jp>
2018-10-22 23:36:08 +0900

    Dollarクラスを作成。 MoneｙTestでは、Dollarクラスがないエラーを解消。

--- TestDrivenDevelopmentUnitTest/MoneyTest.cs
+++ TestDrivenDevelopmentUnitTest/MoneyTest.cs
@@ -1,5 +1,14 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestDrivenDevelopmentPractice;
+
+/*
+ * テストプロジェクトの参照に、テストするプロジェクトの参照を追加すること
+ * 以下、参考情報
+ * https://qiita.com/mima_ita/items/55394bcc851eb8b6dc24
+ * https://qiita.com/mima_ita/items/05ce44c3eb1fd6e9dd46
+ */
+
 
 namespace TestDrivenDevelopmentUnitTest
 {
